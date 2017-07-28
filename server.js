5a19e72f7061150bac37c05172f00bbf4cedc2d3
betsu <bumpchama@gmail.com>
2017-07-28 17:42:26 +0800

    weather code

--- server.js
+++ server.js
@@ -16,10 +16,30 @@ function get() {
     		rawData += chunk;
     	})
     	response.on('end', function(chunk){
-    		var result = JSON.parse(rawData);
-    		
+            var result = JSON.parse(rawData);
+            var data = result['records']['location'];
+            var location = data[0]['parameter'];
+            var weather = data[0]['weatherElement'];
+            var city = location.findIndex(function(item, index){
+                return item['parameterName'] == 'CITY'
+            });
+            var town = location.findIndex(function(item, index){
+                return item['parameterName'] == 'TOWN'
+            });
+            var temp = weather.findIndex(function(item, index){
+                return item['elementName'] == 'TEMP'
+            });
+
+            data.forEach(function(value, index){
+                var cityName = value['parameter'][city]['parameterValue'];
+                var townName =  value['parameter'][town]['parameterValue'];
+                var tempValue = value['weatherElement'][temp]['elementValue'];
+                if(cityName == '臺北市'){
+                    console.log("臺北市 "+ townName +" 溫度 " + tempValue +" 度")
+                }
+            });
     	})
     }).end();
 };
- 
+
 get();
